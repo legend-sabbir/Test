dd9a1d960ca024b3b54c273dfa52a83344616928
Legend Sabbir <sabbirhossain.trash@gmail.com>
2023-01-26 00:51:24 +0600

    first commit

--- functions/api.js
+++ functions/api.js
@@ -3,9 +3,11 @@ const serverless = require('serverless-http');
 const cors = require('cors');
 const app = express();
 const router = express.Router();
+const products = require('./data');
 app.use(cors())
+app.set('view engine', 'ejs');
 
-router.get('/', cors(), (req, res) => {
+router.get('/test', cors(), (req, res) => {
   res.json(
     [{
       'id': '001',
@@ -24,6 +26,35 @@ router.get('/', cors(), (req, res) => {
       }]
   )
 })
+
+router.get('/', (req, res) => {
+  const page = req.query.page || 1;
+  const perPage = 20;
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  
+  const data = [...products.values()].sort((a, b) => {
+    const first = +a.price?.replace(/[,.]/g, "")
+    const last = +b.price?.replace(/[,.]/g, "")
+
+    return last - first
+  }).slice(startIndex, endIndex);
+  res.render('index', {
+    data
+  });
+});
+
+router.get('/product/:productName', (req, res) => {
+  const productName = req.params.productName;
+  const product = products.get(productName);
+  if (!product) {
+    console.log(`Product ${productName} not found.`);
+    res.send("Product not found");
+  } else {
+    res.render('product', { product });
+  }
+});
+
 
 app.use('/', router);
 module.exports.handler = serverless(app);
